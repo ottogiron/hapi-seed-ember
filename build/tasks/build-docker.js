a57a0e82b01d5dcc1e0480a0b3b8cd15d1a5a8bd
Otto Giron <ottog2486@gmail.com>
2015-10-24 00:57:30 -0600

    Fix typo

--- build/tasks/build-docker.js
+++ build/tasks/build-docker.js
@@ -3,7 +3,7 @@ var exec = require('child_process').exec;
 
 var IMAGE_TAG = process.env.DOCKER_IMAGE_TAG || 'ottogiron/hapiember';
 
-gulp.task('build-docker',['build-ember'], function(callback){
+gulp.task('build-docker', ['build-ember'], function(callback){
 
   var dockerBuild = exec('docker build --no-cache -f docker/Dockerfile -t '+ IMAGE_TAG +' .', function(error, stout, stderr) {
 
