40f2b4fbaea43c8bdff686029d11cb2f3e664743
Otto Giron <ottog2486@gmail.com>
2015-11-15 03:24:32 -0600

    New generic ember route

--- routes/ember.js
+++ routes/ember.js
@@ -1,5 +1,12 @@
 
 var Hoek = require('hoek');
+var fs = require('fs');
+var path = require('path');
+
+var internals = {
+  BASE_PATH: 'client/dist',
+  DEFAULT_PATH: 'client/dist/index.html'
+};
 //It shouldn't be necessary for you to modify this file.
 //If you need an additional route consider creating a new one.
 exports.register = function (server, options, next) {
@@ -7,38 +14,27 @@ exports.register = function (server, options, next) {
     server.route({
       method: 'GET',
       path: '/{param*}',
-      handler: {
-          file: 'client/dist/index.html'
-      }
-    });
+      handler: function(request, reply) {
 
-    server.route({
-      method: 'GET',
-      path: '/robots.txt',
-      handler: {
-        file: 'client/dist/robots.txt'
-      }
-    });
-
-    server.route({
-      method: 'GET',
-      path: '/crossdomain.xml',
-      handler: {
-        file: 'client/dist/crossdomain.xml'
-      }
-    });
-
-    server.route({
-      method: 'GET',
-      path: '/assets/{param*}',
-      handler: {
-        directory: {
-          path: 'client/dist/assets'
+        if(request.path === '/' ) {
+          reply.file(internals.DEFAULT_PATH);
+        }
+        else {
+          var resourcePath = path.join(internals.BASE_PATH, request.path);
+          fs.stat(resourcePath, function(err, stat) {
+            if(!err && stat.isFile()){
+              reply.file(resourcePath);
+            }
+            else{
+              reply.file(internals.DEFAULT_PATH);
+            }
+          });
         }
       }
     });
 
     if(process.env.EMBER_ENV === 'development'){
+
       var Yam = require('yam');
       var emberCLI = new Yam('../client/.ember-cli');
       //for dev environment we want to be able to access ember live-reload server
